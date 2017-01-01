fdde923efbc992d261b9456c8120cdd1ee033e97
Udo <noreply@inter.net>
2017-01-01 16:07:46 +0100

    Added second algorithm (Sieve of Erathostenes). There is still some minor bug left.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import random
 import sys
 import os
 
+import math
+
 
 def simple_data_types():
     # Hello world is just one line of code
@@ -231,7 +233,7 @@ def loops():
         print(x)
 
     # You can double up for loops to cycle through lists
-    num_list = [[1, 2, 3], [10, 20, 30], [100, 200, 300]];
+    num_list = [[1, 2, 3], [10, 20, 30], [100, 200, 300]]
 
     for x in range(0, 3):
         for y in range(0, 3):
@@ -247,7 +249,7 @@ def loops():
         random_num = random.randrange(0, 100)
 
     # An iterator for a while loop is defined before the loop
-    i = 0;
+    i = 0
     while (i <= 20):
         if (i % 2 == 0):
             print(i)
@@ -279,7 +281,7 @@ def functions():
     # print(sumNum)
 
     # If you define a variable outside of the function it works every place
-    newNum = 0;
+    newNum = 0
 
     def subNumbers(fNum, sNum):
         newNum = fNum - sNum
@@ -370,7 +372,7 @@ def file_io():
     print(text_in_file)
 
     # Close the file (otherwise it cannot be deleted)
-    test_file.close();
+    test_file.close()
 
     # Delete the file
     os.remove("test.txt")
@@ -494,34 +496,64 @@ def object_oriented_programming():
     spot.multiple_sounds(4)
 
 
-# some conflicting line to try out git rebase
-# (was new comment in master -> we keep it)
+def initialize_bool_array(array_size, initial_value=False):
+    # there must be a way to do that more efficiently
+    array_of_primes = []
+    for i in range(0, array_size + 1):
+        array_of_primes.append(initial_value)
+    return array_of_primes
 
-# main function simply for calling all the demo functions (one for each topic)
-# that way we can have everything in one file
-def generate_prime_naive():
 
-    max_prime = 100
-    print("first calculate prime numbers up to {} using the naive method".format(max_prime))
+def generate_prime_naive(max_prime, print_all_numbers=True):
+    print("calculate prime numbers up to {} using the naive method".format(max_prime))
+
+    # first create and initialize the result array
+    array_of_primes = initialize_bool_array(max_prime)
 
     for i in range(2, max_prime):
 
         # start with the assumption that i is prime and try to disprove it
         is_prime = True
 
-        for j in range(2, i-1):
+        for j in range(2, i - 1):
             if i % j == 0:
                 is_prime = False
                 break
 
         if is_prime:
-            print('{} is a prime number'.format(i))
+            array_of_primes[i] = True
+            if print_all_numbers:
+                print('{} is a prime number'.format(i))
+
+    return array_of_primes
 
 
-def generate_prime_bresenham():
-    pass
+def generate_prime_eratosthenes(max_prime, print_all_numbers=True):
+    print("calculate prime numbers up to {} using the sieve of Eratosthenes".format(max_prime))
+
+    # first create and initialize the result array
+    array_of_primes = initialize_bool_array(max_prime, True)
+
+    # round before converting to int, otherwise the value will be truncated and
+    # might be too small (e.g. int(sqrt(1000)) == int(31.6) == 31 and not 32, as required
+    for i in range(2, int(round(math.sqrt(max_prime)))):
+        if array_of_primes[i]:
+            for j in range(i * i, max_prime + 1, i):
+                array_of_primes[j] = False
+
+    # 0 and 1 are not considered prime numbers
+    array_of_primes[0] = False
+    array_of_primes[1] = False
+    if print_all_numbers:
+        for i in range(2, max_prime):
+            if array_of_primes[i]:
+                print('{} is a prime number'.format(i))
+
+    return array_of_primes
 
 
+# main function simply for calling all the demo functions (one for each topic)
+# that way we can have everything in one file
 def main():
     # the very basics
     # simple_data_types()
@@ -533,8 +565,14 @@ def main():
     # object_oriented_programming()
 
     # 2 functions to generate prime numbers
-    generate_prime_naive()
-    generate_prime_bresenham()
+    max_prime = 1000
+    primes_naive = generate_prime_naive(max_prime, False)
+    primes_eratosthenes = generate_prime_eratosthenes(max_prime, False)
+
+    prime_count_naive = primes_naive.count(True)
+    prime_count_eratosthenes = primes_eratosthenes.count(True)
+    print("Up until {} we have found {} prime numbers with the naive method and {} prime numbers "
+          "using the sieve of Eratosthenes.".format(max_prime, prime_count_naive, prime_count_eratosthenes))
 
 
 if __name__ == "__main__":
