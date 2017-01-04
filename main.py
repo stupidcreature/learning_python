5256a0fd999289e59f0ece8a62023ac39db1c1d7
Udo <noreply@inter.net>
2017-01-04 02:35:24 +0100

    Thread code started

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 # import is used to make specialty functions available
 # These are called modules
 
+import threading
+import time
+import random
 
 # main function simply for calling all the demo functions (one for each topic)
 # that way we can have everything in one file
 from learning_list_comprehension_iterators_and_generators import iterators_list_comprehension_generators
+
+
+def threads():
+    def execute_thread(i):
+        print("Thread {} sleeps at {}".format(i, time.strftime("%H:%M:%S", time.localtime())))
+        rand_sleep_time = random.randint(1, 5)
+        time.sleep(rand_sleep_time)
+        print("Thread {} stops sleeping at {}".format(i, time.strftime("%H:%M:%S", time.localtime())))
+
+    # starts 10 threads
+    for i in range(10):
+        thread = threading.Thread(target=execute_thread, args=(i+1,))
+        thread.start()
+
+    print("Active Threads:", threading.activeCount())
+    print("Thread Objects:", threading.enumerate())
+
+
 
 
 def main():
@@ -19,10 +40,14 @@ def main():
     # object_oriented_programming_intermediate()
     # exceptions()
     # functions_advanced()
-    iterators_list_comprehension_generators()
+    # iterators_list_comprehension_generators()
+    threads()
 
-    # # problems
+
+
+    # # more comprehensive problems (smaller ones are embedded into the functions above
     # primes()
+
 
     pass
 
