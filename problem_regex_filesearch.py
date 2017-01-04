a5b6d40be9352f168092b4891511cceb1d3e459c
Udo <noreply@inter.net>
2017-01-05 00:43:51 +0100

    threading code cleaned up a bit (still not very well structured)

--- problem_regex_filesearch.py
+++ problem_regex_filesearch.py
@@ -1,11 +1,7 @@
 import os
 import re
 import operator
-import threading
-import time
-from collections import deque
 from multiprocessing import Pool, Process, Value, Array
-from functools import partial
 from itertools import repeat
 
 CHARACTERS_FOR_NEAR = 80
@@ -18,8 +14,6 @@ THREADS_FOR_ANALYSES = 8
 def debugprint(*args):
     # print(*args)
     pass
-
-
 
 def get_search_result(contents, searchkey, searchvalue):
 
@@ -77,89 +71,26 @@ def analyze_file(filename, searchdictionary):
 # analyzes files matching filespec (recursively) using criteria given in searchdictionary
 def analyze_files(path, filespec, searchdictionary):
 
-
-    # class AnalysesThread(threading.Thread):
-    #
-    #     def __init__(self, threadid, group=None, target=None, name=None, args=(), kwargs=None, *, daemon=None):
-    #         super().__init__(group, target, name, args, kwargs, daemon=daemon)
-    #         self.__id = threadid
-    #         self.__keeprunning = True
-    #         self.__lock = threading.Lock()
-    #         self.__filequeue = deque()
-    #         self.__result = {}
-    #
-    #     def run(self):
-    #         # first make sure there is some time for the deque to be filled
-    #         # otherwise we will terminate immediately
-    #         time.sleep(1)
-    #
-    #         while self.__keeprunning:
-    #             filename = ""
-    #             self.__lock.acquire()
-    #             if len(self.__filequeue):
-    #                 filename = self.__filequeue.popleft()
-    #             self.__lock.release()
-    #             if filename != "":
-    #                 # print(filename)
-    #                 res = analyze_file(filename)
-    #                 if any(res.values()):
-    #                     print("Thread-ID of thread that received a result:", self.__id, "at",
-    #                           time.strftime("%H:%M:%S", time.localtime()))
-    #                     self.__result.update(res)
-    #             else:
-    #                 # time.sleep(1)
-    #                 self.__keeprunning = False
-    #
-    #     def terminate(self):
-    #         self.__keeprunning = False
-    #
-    #     def add_file_for_analyses(self, filename):
-    #         self.__lock.acquire()
-    #         self.__filequeue.append(filename)
-    #         self.__lock.release()
-    #
-    #     def getresult(self):
-    #         return self.__result
-    #
-    #     ##
-
-    # analyses_thread_pool = []
-    # for i in range(0, THREADS_FOR_ANALYSES):
-    #     analyses_thread_pool.append(AnalysesThread(i))
-    #     analyses_thread_pool[i].start()
-
     result = {}
     filecount = 0
     filenames = []
+
+    # first, collect all filenames to work on
     for root, dirs, files in os.walk(path):
         # filter out files not matching filespec
         files = list(filter(lambda x: re.search(filespec, x), files))
         for name in files:
             filename = os.path.join(root, name)
-            ##
-            # analyses_thread_pool[filecount % THREADS_FOR_ANALYSES].add_file_for_analyses(filename)
             filenames.append(filename)
             filecount += 1
 
-
+    # then span the search threads
     with Pool(THREADS_FOR_ANALYSES) as thread_pool:
         res = thread_pool.starmap(analyze_file, zip(filenames, repeat(searchdictionary)))
         thread_pool.close()
         thread_pool.join()
         for i in res:
             result.update(i)
-
-
-    ##
-    # while any(list(map((lambda x: x.isAlive()), analyses_thread_pool))):
-    #     print("Threads running (total):", threading.activeCount())
-    #     time.sleep(1)
-
-    ##
-    #  wait for all threads to finish their work and collect their results
-    # for i in range(0, THREADS_FOR_ANALYSES):
-    #     analyses_thread_pool[i].join()
-    #     result.update(analyses_thread_pool[i].getresult())
 
     return result
 
@@ -196,16 +127,12 @@ def text_analysis(root_search_path=""):
     if len(root_search_path) == 0:
         root_search_path = r"r:/1/"
 
-    searchdictionary = {r"(testname|derek) NEAR (def)": 0.1, r"import \bre\b": 0.1, r"isinstance NEAR TypeError": 10,
-                        r"def NEAR if": 5}
-    searchdictionary = {r"CacheDispatcher\bThread\b": 1, r"CacheDispatcher": .5}
+    # searchdictionary = {r"(testname|derek) NEAR (def)": 0.1, r"import \bre\b": 0.1, r"isinstance NEAR TypeError": 10, r"def NEAR if": 5}
+    # searchdictionary = {r"CacheDispatcher\bThread\b": 1, r"CacheDispatcher": .5}
 
     searchdictionary = import_search_patterns(r'r:/1/searchdefinitions.txt')
 
-    # result = analyze_files('c:\\dev\\python\\textdatabase\\', ".*py", searchdictionary)
-
     result = analyze_files(root_search_path, r"(txt|TXT|java)$", searchdictionary)
-
     sorted_result = sorted(result.items(), key=operator.itemgetter(1), reverse=True)
 
     for i in sorted_result:
