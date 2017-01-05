845ad9f609df71e8f5119478a67f96cba135fc70
Udo <noreply@inter.net>
2017-01-05 13:56:45 +0100

    working on tkinter

--- learning_tkinter.py
+++ learning_tkinter.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Frame, StringVar, Label, Entry, Button, W, E, S
+from tkinter import *
+from tkinter.ttk import *
 
 
 def tkinter():
@@ -10,11 +11,23 @@ def tkinter():
     label = Label(frame, textvariable=labeltext)
     entry = Entry(frame, width=30)
     button = Button(frame, text="My Button")
+    radio1 = Radiobutton(frame, text="Alternative 1", value=1)
+    radio2 = Radiobutton(frame, text="Alternative 2", value=2)
+    radio3 = Radiobutton(frame, text="Alternative 3", value=3)
+    checkbox1 = Checkbutton(frame, text="Option 1")
+    checkbox2 = Checkbutton(frame, text="Option 2")
     labeltext.set("My Label")
 
     label.grid(row=0, column=0, sticky=W, padx=5)
     entry.grid(row=0, column=1, sticky=E, pady=10)
-    button.grid(row=1, column=1, sticky=S, padx=20, pady=5)
+    button.grid(row=1, column=0, columnspan=2, sticky=S + E + W, padx=20, pady=5)
+
+    radio1.grid(row=4, column=0, sticky=W, padx=5)
+    radio2.grid(row=5, column=0, sticky=W, padx=5)
+    radio3.grid(row=6, column=0, sticky=W, padx=5)
+    checkbox1.grid(row=4, column=1, sticky=E, padx=5)
+    checkbox2.grid(row=5, column=1, sticky=E, padx=5)
+
     frame.grid()
 
     root.mainloop()
